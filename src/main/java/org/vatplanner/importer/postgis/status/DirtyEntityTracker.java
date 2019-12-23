b813a3705a48cf5e2bfd3c00efe0f35cafbb854f
Daniel Neugebauer <dneuge@energiequant.de>
2019-12-23 10:59:36 +0100

    DirtyEntityTracker: refactoring

--- src/main/java/org/vatplanner/importer/postgis/status/DirtyEntityTracker.java
+++ src/main/java/org/vatplanner/importer/postgis/status/DirtyEntityTracker.java
@@ -11,19 +11,24 @@ public class DirtyEntityTracker {
 
     private final Map<Class<? extends DirtyMark>, Set<DirtyMark>> dirtyEntitiesByClass = new HashMap<>();
 
+    private <T extends DirtyMark> Set<T> access(Class<T> entityClass) {
+        return (Set<T>) dirtyEntitiesByClass.computeIfAbsent(entityClass, c -> new HashSet<>());
+    }
+
     public <T extends DirtyMark> void recordAsDirty(Class<T> entityClass, T entity) {
-        Set<DirtyMark> dirtyEntities = dirtyEntitiesByClass.computeIfAbsent(entityClass, c -> new HashSet<>());
-        dirtyEntities.add(entity);
+        access(entityClass).add(entity);
     }
 
     public <T extends DirtyMark> void recordAsClean(Class<T> entityClass, T entity) {
-        Set<DirtyMark> dirtyEntities = dirtyEntitiesByClass.computeIfAbsent(entityClass, c -> new HashSet<>());
-        dirtyEntities.remove(entity);
+        access(entityClass).remove(entity);
     }
 
     public <T extends DirtyMark> Set<T> getDirtyEntities(Class<T> entityClass) {
-        Set<T> dirtyEntities = (Set<T>) dirtyEntitiesByClass.computeIfAbsent(entityClass, c -> new HashSet<>());
-        return Collections.unmodifiableSet(new HashSet<>(dirtyEntities));
+        return Collections.unmodifiableSet(new HashSet<>(access(entityClass)));
+    }
+
+    public <T extends DirtyMark> boolean isDirty(Class<T> entityClass, T entity) {
+        return access(entityClass).contains(entity);
     }
 
     public int countDirtyEntities() {
